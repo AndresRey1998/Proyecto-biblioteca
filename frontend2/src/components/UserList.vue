59894cc93342151f348fc257b0ed09c529fa3191
Jonathan <36428207+Natsume-197@users.noreply.github.com>
2021-12-02 22:18:41 -0500

    CRUD parcial

--- frontend2/src/components/UserList.vue
+++ frontend2/src/components/UserList.vue
@@ -4,13 +4,43 @@
       :columns="columns"
       :rows="users"
       :row-style-class="rowStyleClassFn"
-      :paginate="true">
+      :paginate="true"
+      :responsive="true">
+          <template #table-row="props">
+                <span v-if="props.column.field == 'actions'">
+                  <button size="sm" @click="modalEdit(props)" variant="primary" class="mr-2" title="Edit">
+                    <i>
+                        <font-awesome-icon icon="edit" /> Edit
+                      </i>
+                  </button>
+                  <button size="sm" variant="danger" class="mr-2" title="Hapus">
+                    <i>
+                        <font-awesome-icon icon="trash" /> Disable
+                      </i>
+                  </button>
+                </span>
+                    <span v-if="props.column.field == 'state'">
+                      <span v-if="props.row.state == 'Activated'">
+                        <span style="font-weight: bold; color: green;">{{props.row.state}}</span> 
+                      </span>
+                      <span v-else>
+                        <span style="font-weight: bold; color: red;">{{props.row.state}}</span> 
+                    </span>
+                  </span>
+                  
+                <span v-else>
+                  {{props.formattedRow[props.column.field]}}
+                </span>
+              </template>
    </vue-good-table>
  </div>
 </template>
 
 <script>
 import UserDataService from "../services/userData.service";
+import { openModal } from "jenesius-vue-modal";
+import ModalEditUser from './modals/ModalEditUser.vue'
+
 export default {
   name: "users-list",
   data() {
@@ -21,11 +51,22 @@ export default {
         { label: "Username", field: "username" },
         { label: "Email", field: "email" },
         { label: "State", field: "state" },
+        { label: "Permissions", field: "roles" },
         { label: "Actions", field: "actions", sortable: false}
       ],
     };
   },
   methods: {
+    modalEdit(props){
+      openModal(ModalEditUser, {
+        username: props.row.username,
+        email: props.row.email,
+        roles: this.users.roles
+      })
+      
+      document.getElementsByTagName('body')[0].style = 'overflow:hidden;';
+  },
+
   rowStyleClassFn(row) {
     console.log(row)
     return row.state === true ? row.state="Activated" : 'Not Activated';
@@ -46,7 +87,7 @@ export default {
     },
 
     editUser(id) {
-      this.$router.push({ name: "user-details", params: { id: id } });
+      console.log(id)
     },
 
     deleteUser(id) {
@@ -64,7 +105,8 @@ export default {
         id: user.id,
         username: user.userName.length > 30 ? user.userName.substr(0, 30) + "..." : user.userName,
         email: user.email.length > 30 ? user.email.substr(0, 30) + "..." : user.email,
-        state: user.enable
+        state: user.enable,
+        roles: user.roles
       };
     },
   },
